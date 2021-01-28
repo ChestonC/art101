c942eb2d5ac3ccafe9c0c2c6d41d1a326c6b6145
ChestonC <60534058+ChestonC@users.noreply.github.com>
2021-01-28 02:01:14 -0800

    5

---
+++ lab5/js/lab.js
@@ -0,0 +1,18 @@
+//Cheston Chen and Brian Quintero
+//Lab 5
+//Data Types and Variables
+//1/26/2021
+
+//Declaring variables.
+var carMake = "Toyota";
+var carModel= "Matrix";
+var carColor= "Light blue";
+var carYear= 2008;
+var age = 2020 - carYear;
+
+//Output
+document.writeln("Make: " + carMake + "<br>");
+document.writeln("Model: " + carModel + "<br>");
+document.writeln("Color: " + carColor + "<br>");
+document.writeln("Year: " + carYear + "<br>");
+document.writeln("Age: " + age + " years<br>");
